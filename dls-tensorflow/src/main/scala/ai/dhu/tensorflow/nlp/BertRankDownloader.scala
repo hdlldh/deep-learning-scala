09abd13c2637ab9935e8807b20012ec9deb96193
Donglin Hu <donglin.hu@salesforce.com>
2022-06-13 12:07:46 -0700

    Fix downloaded file name

--- dls-tensorflow/src/main/scala/ai/dhu/tensorflow/nlp/BertRankDownloader.scala
+++ dls-tensorflow/src/main/scala/ai/dhu/tensorflow/nlp/BertRankDownloader.scala
@@ -10,7 +10,7 @@ object BertRankDownloader {
   def main(args: Array[String]): Unit = {
     val modelUrl = "https://resources.djl.ai/demo/tensorflow/amazon_review_rank_classification.zip"
     DownloadUtils.download(modelUrl, "build/tensorflow/amazon_review_rank_classification.zip", new ProgressBar)
-    val zipFile = Paths.get("build/tensorflow/bert_review_rank_classification.zip")
+    val zipFile = Paths.get("build/tensorflow/amazon_review_rank_classification.zip")
 
     val modelDir = Paths.get("build/tensorflow/bert-rank")
     if (Files.notExists(modelDir)) ZipUtils.unzip(Files.newInputStream(zipFile), modelDir)
