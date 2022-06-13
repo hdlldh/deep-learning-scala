c0b7e3a12e052a601005426442b6972e14f18609
Donglin Hu <donglin.hu@salesforce.com>
2022-06-13 12:33:06 -0700

    Fix path and file name for pytorch

--- dls-pytorch/src/main/scala/ai/dhu/pytorch/nlp/BertQuAnDownloader.scala
+++ dls-pytorch/src/main/scala/ai/dhu/pytorch/nlp/BertQuAnDownloader.scala
@@ -8,12 +8,12 @@ object BertQuAnDownloader {
 
     DownloadUtils.download(
       "https://djl-ai.s3.amazonaws.com/mlrepo/model/nlp/question_answer/ai/djl/pytorch/bertqa/0.0.1/bert-base-uncased-vocab.txt.gz",
-      "build/pytorch/bertqa/vocab.txt",
+      "build/pytorch/bert-qa/vocab.txt",
       new ProgressBar)
 
     DownloadUtils.download(
       "https://djl-ai.s3.amazonaws.com/mlrepo/model/nlp/question_answer/ai/djl/pytorch/bertqa/0.0.1/trace_bertqa.pt.gz",
-      "build/pytorch/bertqa/bertqa.pt",
+      "build/pytorch/bert-qa/bert-qa.pt",
       new ProgressBar)
   }
 
