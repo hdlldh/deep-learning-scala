af41eb3c1c57f61f8a81ebdd53a845bff635911e
donglin <hdlldh@gmail.com>
2022-07-08 23:46:47 -0700

    refactor model downloader

---
+++ utils/python/HfBaseModelType.py
@@ -0,0 +1,8 @@
+class HfBaseModelType(object):
+    TEXT_CLASSIFICATION = "text-classification"
+    TOKEN_CLASSIFICATION = "token-classification"
+    QUESTION_ANSWERING = "question-answering"
+    FILL_MASK = "fill-mask"
+    CAUSAL_LM = "causal-lm"
+    ZERO_SHOT_CLASSIFICATION = "zero-shot-classification"
+
